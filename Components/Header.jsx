e074fb088f88f543125a76a0507a4c62232d590b
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-14 21:57:40 +0100

    prova16

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -6,8 +6,8 @@ import { useSession, signOut } from "next-auth/react";
 const Header = () => {
     const { data: session, status } = useSession();
   return (
-    <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
-      <div className="flex items-center justify-between px-6 py-4 max-w-6xl mx-auto rounded-lg">
+    <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-md flex items-center justify-between px-6 py-4 max-w-6xl mx-auto rounded-lg">
+      <div className="">
         {/* Logo */}
         <div className="flex items-center">
           <Link href={"/"}>
