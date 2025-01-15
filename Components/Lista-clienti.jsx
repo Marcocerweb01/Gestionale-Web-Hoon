30b88758449ab6b0c0d8c14d0dea9b9cd873eb3d
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-15 17:24:34 +0100

    prova27

--- Components/Lista-clienti.jsx
+++ Components/Lista-clienti.jsx
@@ -149,7 +149,7 @@ const ListaClienti = ({ id, amministratore }) => {
 
   // Componente per la visualizzazione desktop
   const DesktopView = () => (
-    <div className="hidden sm:hidden lg:hidden overflow-x-auto">
+    <div className="hidden sm:hidden overflow-x-auto">
       <table className="w-full border border-black text-left">
         <thead className="rounded-lg">
           <tr className="bg-gray-100">
@@ -330,8 +330,9 @@ const ListaClienti = ({ id, amministratore }) => {
 
   return (
     <>
-      <DesktopView />
-      <MobileView />
+      {amministratore?<DesktopView />:<MobileView />}
+      
+      
     </>
   );
 };
