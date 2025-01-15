5046cab4417e0bcffc35155aceb3ccef1c1ba074
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-15 17:58:00 +0100

    prova27

--- Components/feed-commerciale.jsx
+++ Components/feed-commerciale.jsx
@@ -38,7 +38,7 @@ const FeedCommerciale = ({ id }) => {
   };
 
   return (
-    <div className="relative w-full h-full" >
+    <div className="relative w-full h-3/5" >
       <div className="flex justify-between items-center mb-4" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <h2 className="text-xl font-bold">Feed delle Note</h2>
         <button
