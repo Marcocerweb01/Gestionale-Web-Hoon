d2db0073c29f6c22df62db8d7d2e08cf01e17810
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-15 17:43:47 +0100

    prova27

--- Components/feed-commerciale.jsx
+++ Components/feed-commerciale.jsx
@@ -142,7 +142,7 @@ const PopupForm = ({ onClose, onAddNote, autoreId, autoreNome }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-150">
       <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
         <div className="flex flex-row mb-5">
         <h3 className="text-xl font-bold w-5/6">Crea Nota</h3>
