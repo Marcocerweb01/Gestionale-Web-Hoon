c9c12ce5fc1e6bc32f8fba48befe9d507569e680
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-16 14:42:38 +0100

    sistemazione layoute resp

--- app/Feed-2/[collaboratoreId]/page.jsx
+++ app/Feed-2/[collaboratoreId]/page.jsx
@@ -92,7 +92,7 @@ useEffect(() => {
 
       {/* Sidebar responsive */}
       <div className={`
-        fixed left-0 top-24 sm:top-32 md:top-32 bottom-0 z-10 w-64 bg-gray-200 p-4 transition-transform duration-300 ease-in-out
+        fixed left-0 top-32 sm:top-32 md:top-32 bottom-0 z-10 w-64 bg-gray-200 p-4 transition-transform duration-300 ease-in-out
         lg:translate-x-0 lg:w-1/4
         ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}
       `}>
