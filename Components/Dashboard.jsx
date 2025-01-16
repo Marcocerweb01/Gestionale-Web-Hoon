d495539a19ca12943371ff2d483cb9df274d2dc6
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-16 14:14:01 +0100

    sistemazione layoute resp

--- Components/Dashboard.jsx
+++ Components/Dashboard.jsx
@@ -51,7 +51,7 @@ const Dashboard = () => {
             
     
     <div className="w-full h-full bg-slate-50 shadow-md rounded-lg mt-10 ">
-      <h1 className='head_text'>Ciao {session?.user?.nome}</h1>
+      <h1 className='head_text p-5'>Ciao {session?.user?.nome}</h1>
       
 
       <div className="flex space-x-5"> 
