88213615fb42ded185bc7374491163895bc231e7
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-01-16 14:28:18 +0100

    sistemazione layoute resp

--- Components/feed-commerciale.jsx
+++ Components/feed-commerciale.jsx
@@ -257,7 +257,18 @@ const PopupForm = ({ onClose, onAddNote, autoreId, autoreNome }) => {
       <button
         onClick={onClose}
         className="text-gray-500 hover:text-gray-800 w-1/6 text-4xl flex items-center align-middle justify-center"
-      >
+        style={{
+          color: 'rgb(107, 114, 128)', // text-gray-500
+          hover: {
+            color: 'rgb(31, 41, 55)', // hover:text-gray-800
+          },
+          width: '16.666667%', // w-1/6
+          fontSize: '2.25rem', // text-4xl
+          display: 'flex', // flex
+          alignItems: 'center', // items-center
+          justifyContent: 'center', // justify-center
+          verticalAlign: 'middle', // align-middle
+        }}>
         &times;
       </button>
     </div>
