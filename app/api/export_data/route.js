19df23dc8dfbe84290815585eec78c0d826aab30
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-04-01 10:39:02 +0200

    download

--- app/api/export_data/route.js
+++ app/api/export_data/route.js
@@ -18,10 +18,9 @@ export async function GET() {
       "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
     ];
     const monthYearTitle = `${italianMonths[now.getMonth()]} ${now.getFullYear()}`;
-    //const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-    //const lastDayOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-    const firstDayOfMonth = new Date(2025, 2, 1); // Marzo è mese 2 (gennaio = 0)
-    const lastDayOfMonth = new Date(2025, 2, 31);
+    const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth() - 1, 1);
+    const lastDayOfMonth = new Date(now.getFullYear(), now.getMonth() , 0);
+  
 
     // Prepara i dati da inserire: per ogni collaborazione, contiamo le note di tipo "appuntamento"
     const rowsData = await Promise.all(
