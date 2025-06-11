c275c107b3c4850fa9db9aa6c751dac0dda469bf
Marcocerweb01 <marcocerasaweb@gmail.com>
2025-06-11 11:55:58 +0200

    fix

--- app/Pagamenti/page.jsx
+++ app/Pagamenti/page.jsx
@@ -5,12 +5,8 @@ const page = async () => {
   
   return (
     <div>
-        <h1>Pagamenti</h1>
-        <p>Questa è la pagina dei pagamenti.</p>
-        <p>Qui puoi visualizzare e gestire i pagamenti effettuati dai clienti.</p>
-        <p>Puoi anche aggiungere nuovi pagamenti e modificare quelli esistenti.</p>
-        <p>Utilizza il menu a sinistra per navigare tra le diverse sezioni.</p>
-        <p>Se hai bisogno di assistenza, contatta il supporto.</p>
+        <h1 className='head_text p-5'>Pagamenti</h1>
+  
 
         <PagamentiTable />
         
